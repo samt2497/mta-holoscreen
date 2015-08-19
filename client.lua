89af1b032a2ce6511d5c3d901983f04e267b83d3
samt2497 <samt2497@hotmail.com>
2015-08-18 21:59:54 -0500

    Performance update
    Improved performance for the resource.
    The rendertarget gets created when player is near and auto unloaded.
    Holorender event sends rendertarget argument.
    Lowered the pixels per mta unit.
    Fixed an issue with the matrix when the holoscreen rotation is 0

--- client.lua
+++ client.lua
@@ -1,50 +1,40 @@
 prefix_RTID = "hsrt_"
-PixPerUnit = 200
-RenderDistance = 20
+PixPerUnit = 100
+RenderDistance = 30
+ActionDistance = 10
 RTCache = {}
 ix = 0
 
 
 addEvent ( "onHoloScrenRender", true )
 
-function createHoloScreen(x,y,z,rx,ry,rz,height,width,may_rt,may_hg)
-	local RT_ID = nil
+function getHRTE(holoscreen)
+	if RTCache[holoscreen] then
+		RTCache[holoscreen].stamp = getTickCount()
+	else
+		RTCache[holoscreen] = {}
+		RTCache[holoscreen].RTE = dxCreateRenderTarget(getElementData(holoscreen,"rtw"),getElementData(holoscreen,"rth"),true)
+		RTCache[holoscreen].stamp = getTickCount()
+	end
+	return RTCache[holoscreen].RTE
+end
+
+function createHoloScreen(x,y,z,rx,ry,rz,height,width)
 	local created = nil
-	if type(may_rt) == "string" then
-		local maye = getElementByID(may_rt)
-		if isElement(maye) then
-			RT_ID = maye
-		end
-	end
-	if not RT_ID then
-		if isElement(may_rt) then
-			RT_ID = getElementID(may_rt)
-		else
-			local rtw = tonumber(may_rt) or 0
-			local rth = tonumber(may_hg) or 0
-			if rtw < 1 then
-				rtw = width*PixPerUnit
-			end
-			if rth < 1 then
-				rth = width*PixPerUnit
-			end
-			local crt = dxCreateRenderTarget(rtw,rth,true)
-			ix = ix + 1
-			RT_ID = prefix_RTID..ix
-			setElementID(crt,RT_ID)
-		end
-	end
-	local RTE = getElementByID(RT_ID)
-	if isElement(RTE) then
-		local holoscreen = createElement("holoscreen")
-		setElementData(holoscreen,"RTID",RT_ID,false)
+	local rtw,rth
+	rtw = width*PixPerUnit
+	rth = height*PixPerUnit
+	local holoscreen = createElement("holoscreen")
+	if holoscreen then
 		setElementData(holoscreen,"height",height,false)
 		setElementData(holoscreen,"width",width,false)
+		setElementData(holoscreen,"rtw",rtw,false)
+		setElementData(holoscreen,"rth",rth,false)
 		setElementPosition(holoscreen,x,y,z)
 		setElementData(holoscreen,"rx",rx,false)
 		setElementData(holoscreen,"ry",ry,false)
 		setElementData(holoscreen,"rz",rz,false)
-		return holoscreen,RT_ID,RTE
+		return holoscreen,rtw,rth
 	else
 		return false
 	end
@@ -54,8 +44,7 @@ end
 addEventHandler("onClientElementDataChange", getRootElement(),
 function(dataName)
 	if getElementType(source) == "holoscreen" then
-		if dataName == "RTID" then
-			RTCache[source] = getElementData(source,dataName)
+		if dataName == "RTID" then 
 		end
 	end
 end)
@@ -72,12 +61,13 @@ function getOffsetFromXYZ( mat, vec )
     mat[2][4] = 0
     mat[3][4] = 0
     mat[4][4] = 1
-    mat = matrix.invert(mat)
+    mat = matrix.invert( mat )
     local offX = vec[1] * mat[1][1] + vec[2] * mat[2][1] + vec[3] * mat[3][1] + mat[4][1]
     local offY = vec[1] * mat[1][2] + vec[2] * mat[2][2] + vec[3] * mat[3][2] + mat[4][2]
     local offZ = vec[1] * mat[1][3] + vec[2] * mat[2][3] + vec[3] * mat[3][3] + mat[4][3]
     return offX, offY, offZ
 end
+
 function math.lerp(from,to,progress)
     return from + (to-from) * progress
 end
@@ -87,32 +77,51 @@ end
 
 function getEElementMatrix(element)
 	local rx, ry, rz = getElementData(element,"rx"), getElementData(element,"ry"), getElementData(element,"rz")
+	if rx == 0 then
+		rx = 0.00001
+	end
+	if ry == 0 then
+		ry = 0.00001
+	end
+		if rz == 0 then
+		rz = 0.00001
+	end
 	rx, ry, rz = math.rad(rx), math.rad(ry), math.rad(rz)
 	local matrix = {}
-	matrix[1] = {}
-	matrix[1][1] = math.cos(rz)*math.cos(ry) - math.sin(rz)*math.sin(rx)*math.sin(ry)
-	matrix[1][2] = math.cos(ry)*math.sin(rz) + math.cos(rz)*math.sin(rx)*math.sin(ry)
-	matrix[1][3] = -math.cos(rx)*math.sin(ry)
-	matrix[1][4] = 1
+    matrix[1] = {}
+    matrix[1][1] = math.cos(rz)*math.cos(ry) - math.sin(rz)*math.sin(rx)*math.sin(ry)
+    matrix[1][2] = math.cos(ry)*math.sin(rz) + math.cos(rz)*math.sin(rx)*math.sin(ry)
+    matrix[1][3] = -math.cos(rx)*math.sin(ry)
+    matrix[1][4] = 1
  
-	matrix[2] = {}
-	matrix[2][1] = -math.cos(rx)*math.sin(rz)
-	matrix[2][2] = math.cos(rz)*math.cos(rx)
-	matrix[2][3] = math.sin(rx)
-	matrix[2][4] = 1
+    matrix[2] = {}
+    matrix[2][1] = -math.cos(rx)*math.sin(rz)
+    matrix[2][2] = math.cos(rz)*math.cos(rx)
+    matrix[2][3] = math.sin(rx)
+    matrix[2][4] = 1
  
-	matrix[3] = {}
-	matrix[3][1] = math.cos(rz)*math.sin(ry) + math.cos(ry)*math.sin(rz)*math.sin(rx)
-	matrix[3][2] = math.sin(rz)*math.sin(ry) - math.cos(rz)*math.cos(ry)*math.sin(rx)
-	matrix[3][3] = math.cos(rx)*math.cos(ry)
-	matrix[3][4] = 1
+    matrix[3] = {}
+    matrix[3][1] = math.cos(rz)*math.sin(ry) + math.cos(ry)*math.sin(rz)*math.sin(rx)
+    matrix[3][2] = math.sin(rz)*math.sin(ry) - math.cos(rz)*math.cos(ry)*math.sin(rx)
+    matrix[3][3] = math.cos(rx)*math.cos(ry)
+    matrix[3][4] = 1
  
-	matrix[4] = {}
-	matrix[4][1], matrix[4][2], matrix[4][3] = getElementPosition(element)
-	matrix[4][4] = 1
+    matrix[4] = {}
+    matrix[4][1], matrix[4][2], matrix[4][3] = getElementPosition(element)
+    matrix[4][4] = 1
  
 	return matrix
 end
+
+function GarbageCollector()
+	for holo,vals in pairs(RTCache) do
+		if getTickCount()-vals.stamp > 5000 then
+			destroyElement(vals.RTE)
+			RTCache[holo] = nil
+		end
+	end
+end
+setTimer(GarbageCollector,3000,0)
 
 function renderHoloScreens()
 	local cx, cy, cz, clx, cly, clz = getCameraMatrix()
@@ -127,30 +136,32 @@ function renderHoloScreens()
 			local ix,iy,iz = getPositionFromMatrixOffset(matrix,0,0,hs_height*.5)
 			local ex,ey,ez = getPositionFromMatrixOffset(matrix,0,0,-hs_height*.5)
 			local fx,fy,fz = getPositionFromMatrixOffset(matrix,0,hs_width,0)
-			RTE = getElementByID(getElementData(holoscreen,"RTID"))
+			RTE = getHRTE(holoscreen)
 			-- Test Interactive
-			local interactive = getElementData(holoscreen,"interactive")
-			if interactive then
-				if interactive == "aim" then
-					cstate = isPedDoingTask(localPlayer,"TASK_SIMPLE_USE_GUN")
-					if cstate then						
-						local tx,ty,tz = getPedTargetCollision(localPlayer)
-						if not tx then
-							tx,ty,tz =  getPedTargetEnd(localPlayer)
-						end
-						local ofx,ofy,ofz = getOffsetFromXYZ(matrix,{cx, cy, cz})
-						local efx,efy,efz = getOffsetFromXYZ(matrix,{tx, ty, tz})
-						local progress = math.unlerp(ofy,efy,0)
-						local ix,iy,iz = interpolateBetween (ofx,ofy,ofz,efx,efy,efz,progress,"Linear")	
-						if (ix > -hs_width*.5 and ix < hs_width*.5) and (iy > -hs_height*.5 and iy < hs_height*.5) then
-							setElementData(holoscreen,"ix",(-ix+(hs_width*.5))/hs_width)
-							setElementData(holoscreen,"iy",(-iz+(hs_height*.5))/hs_height)
+			if hs_distance < ActionDistance then
+				local interactive = getElementData(holoscreen,"interactive")
+				if interactive then
+					if interactive == "aim" then
+						cstate = isPedDoingTask(localPlayer,"TASK_SIMPLE_USE_GUN")
+						if cstate then				
+							local tx,ty,tz = getPedTargetCollision(localPlayer)
+							if not tx then
+								tx,ty,tz =  getPedTargetEnd(localPlayer)
+							end
+							local ofx,ofy,ofz = getOffsetFromXYZ(matrix,{cx, cy, cz})
+							local efx,efy,efz = getOffsetFromXYZ(matrix,{tx, ty, tz})
+							local progress = math.unlerp(ofy,efy,0)
+							local ix,iy,iz = interpolateBetween (ofx,ofy,ofz,efx,efy,efz,progress,"Linear")	
+							if (ix > -hs_width*.5 and ix < hs_width*.5) and (iy > -hs_height*.5 and iy < hs_height*.5) then
+								setElementData(holoscreen,"ix",(-ix+(hs_width*.5))/hs_width)
+								setElementData(holoscreen,"iy",(-iz+(hs_height*.5))/hs_height)
+							end
 						end
 					end
 				end
 			end
 			--Send Signal
-			triggerEvent ("onHoloScrenRender",holoscreen)
+			triggerEvent ("onHoloScrenRender",holoscreen,RTE)
 			--Draw IT			
 			dxDrawMaterialLine3D (ix,iy,iz,ex,ey,ez,RTE,hs_width,white,fx,fy,fz)
 		end
--- demo.lua
+++ demo.lua
@@ -1,31 +1,27 @@
-RTE_Demo = nil
-RTE_Holo = nil
+local holo
+local RTE_w = 0
+local RTE_h = 0
 
 addEventHandler( "onClientResourceStart", resourceRoot,
     function ( startedRes )
-        local holoscreen,RT_ID,RTE = createHoloScreen(149,2482,16.5,40,0,90,1.35,2) --Create our holoscreen
-		setElementData(holoscreen,"interactive","aim") -- Make it interactive on user aim
-		if RTE then -- If we got the render Target of the screen then
-			RTE_Demo = RTE
-			RTE_Holo = holoscreen
-			addEventHandler ( "onHoloScrenRender", RTE_Holo,drawHoloDemo) -- Event handler for every render of the holo screen
-		end
+       holo,RTE_w,RTE_h = createHoloScreen(149,2482,16.5,40,0,90,1.35,2) --Create our holoscreen
+		setElementData(holo,"interactive","aim") 
+		addEventHandler ( "onHoloScrenRender", holo,drawHoloDemo)
     end
 );
 
-function drawHoloDemo()
-	local width, height = dxGetMaterialSize(RTE_Demo)
-	dxSetRenderTarget(RTE_Demo,true)
+function drawHoloDemo(rendertarget)
+	dxSetRenderTarget(rendertarget,true)
 	--
-		dxDrawRectangle (0,0,10,1000,tocolor(0,255,0,200),false)
-		dxDrawRectangle (0,0,1000,1000,tocolor(0,255,255,200),false)
-		local ix = getElementData(RTE_Holo,"ix")
-		local iy = getElementData(RTE_Holo,"iy")
-		if ix and iy then
-			local tx = ix*width
-			local ty = iy*height
-			dxDrawLine (tx-13,ty, tx+13,ty,tocolor(255,0,0,255),1)
-			dxDrawLine (tx,ty-13,tx,ty+13,tocolor(255,0,0,255),1)
+		dxDrawRectangle (0,0,10,RTE_h,tocolor(0,255,0,200),false)
+		dxDrawRectangle (0,0,RTE_w,RTE_h,tocolor(0,255,255,200),false)
+		local ix = (getElementData(holo,"ix") or -1)*RTE_w
+		local iy = (getElementData(holo,"iy") or -1)*RTE_h
+		if ix > 0 and iy > 0 then
+			local tx = ix
+			local ty = iy
+			dxDrawLine (tx-8,ty, tx+8,ty,tocolor(255,255,255,255),3)
+			dxDrawLine (tx,ty-8,tx,ty+8,tocolor(255,255,255,255),3)
 		end
 	--
 	dxSetRenderTarget()
